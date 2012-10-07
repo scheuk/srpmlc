6f3667918c4b5f8213288c1375a13d48a74489c2
Kevin Scheunemann <kevin@nanbox.net>
2012-10-07 14:54:16 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Srpmlc::Application.config.secret_token = '133659d7d8f17bd9754950c6613073a3da36200d5e5217f65ec449a7bb5b9f9c72cc126696df5300b37b5dd18f500fc0066d170b708f36be46c7be86e5430019'
